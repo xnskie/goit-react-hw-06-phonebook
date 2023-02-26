cb68dcec7c023f73b643125915ea5cab4024859b
Vadym Surovytskyi <vsurovytskyi@gmail.com>
2023-02-26 17:58:44 +0200

    k

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { setFilter } from 'redux/filter/filter-slice';
-import { useSelector, useDispatch } from 'react-redux';
-import { getFilter } from 'redux/filter/filter-selectors';
+import { useDispatch } from 'react-redux';
+// import { getFilter } from 'redux/filter/filter-selectors';
 import { Input } from './Filter.styled';
 
 function Filter () {
